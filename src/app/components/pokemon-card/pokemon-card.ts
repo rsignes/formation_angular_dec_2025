2861301f0fe42a83b9172cc14f918bb41242c81b
romsinge <romain.signes@gmail.com>
2025-11-30 22:48:23 +0100

    pokemon-card component

---
+++ src/app/components/pokemon-card/pokemon-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-card',
+  imports: [],
+  templateUrl: './pokemon-card.html',
+  styleUrl: './pokemon-card.scss',
+})
+export class PokemonCard {
+
+}
