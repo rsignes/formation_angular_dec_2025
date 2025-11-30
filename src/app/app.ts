31971ae9902f5b9dd88e564c23e2c65c06496a2b
romsinge <romain.signes@gmail.com>
2025-12-01 00:23:39 +0100

    used control flow for to display cards

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,21 @@ import { PokemonCard } from './components/pokemon-card/pokemon-card';
 })
 export class App {
   protected readonly title = signal('formation_angular_dec_2025');
+
+  pokemons = [
+    {
+      name: 'Pikachu',
+      type: 'Électrique',
+      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
+      weight: 6,
+      size: 0.4,
+    },
+    {
+      name: 'Bulbizarre',
+      type: 'Plante',
+      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+      weight: 6.9,
+      size: 0.7,
+    },
+  ];
 }
