a31a058f89f6c635fc8d8de066583bd8ff534c69
romsinge <romain.signes@gmail.com>
2025-11-30 23:27:08 +0100

    material + pokemon-card uses mat-card and displays pikachu

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PokemonCard } from './components/pokemon-card/pokemon-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PokemonCard],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('formation_angular_dec_2025');
--- src/app/components/pokemon-card/pokemon-card.ts
+++ src/app/components/pokemon-card/pokemon-card.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {
+  MatCard,
+  MatCardHeader,
+  MatCardTitle,
+  MatCardSubtitle,
+  MatCardContent,
+} from '@angular/material/card';
 
 @Component({
   selector: 'app-pokemon-card',
-  imports: [],
+  imports: [MatCard, MatCardHeader, MatCardTitle, MatCardSubtitle, MatCardContent],
   templateUrl: './pokemon-card.html',
   styleUrl: './pokemon-card.scss',
 })
-export class PokemonCard {
-
-}
+export class PokemonCard {}
