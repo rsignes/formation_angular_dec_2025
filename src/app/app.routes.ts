fe5adec90e38baa51c9e8e54ca6734a33e7d9510
romsinge <romain.signes@gmail.com>
2025-12-01 17:33:06 +0100

    login form with validators

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { PokemonList } from './components/pokemon-list/pokemon-list';
 import { PokemonDetails } from './components/pokemon-details/pokemon-details';
+import { Login } from './components/login/login';
 
 export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login,
+  },
   {
     path: 'home',
     component: PokemonList,
---
+++ src/app/components/login/login.ts
@@ -0,0 +1,75 @@
+import { Component, inject } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { MatError, MatFormField, MatInput, MatLabel } from '@angular/material/input';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    ReactiveFormsModule,
+    MatInput,
+    MatError,
+    MatFormField,
+    MatButton,
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    MatLabel,
+    MatIcon,
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+  private fb = inject(FormBuilder);
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, this.createPasswordStrengthValidator()]],
+  });
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Formulaire valide, données soumises :', this.loginForm.value);
+    } else {
+      console.error('Le formulaire contient des erreurs.');
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+  createPasswordStrengthValidator(): ValidationErrors | null {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+
+      if (!value) {
+        return null;
+      }
+
+      const hasUpperCase = /[A-Z]+/.test(value);
+      const hasLowerCase = /[a-z]+/.test(value);
+      const hasNumeric = /[0-9]+/.test(value);
+
+      const isStrong = hasUpperCase && hasLowerCase && hasNumeric && value.length >= 4;
+
+      return !isStrong ? { passwordStrength: true } : null;
+    };
+  }
+}
